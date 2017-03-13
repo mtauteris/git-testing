9030065ec779c557f9769f7bfc5e1e07b33b7691
Mindaugas <mindaugas.tauteris@gmail.com>
2017-03-13 12:53:46 +0200

    new input functionality

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Ivesk bet ka");
+		reader.hasNext();
+	}
+
+}
